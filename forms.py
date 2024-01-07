18bf5c9668f7f398cafa5a09874639fe2a4b4cc2
justhosi <44212529+justhosi@users.noreply.github.com>
2024-01-07 18:15:44 +0300

    add login function and profile page

--- forms.py
+++ forms.py
@@ -10,3 +10,15 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Choose a password', validators=[DataRequired(), length(min=3)])
     confirm_password = PasswordField('Confirm your password', validators=[EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
+
+class UpdateForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    username = StringField('Choose a username', validators=[DataRequired()])
+    email = StringField('Please enter your email address', validators=[Email()])
+    color = SelectField(u'What is your favorite color?', choices=[('F6F7C4', 'Yellow'), ('FF6969', 'Red'), ('AEE2FF', 'Blue'), ('D0F5BE', 'Green')])
+    submit = SubmitField('Update')
+
+class LoginForm(FlaskForm):
+    email = StringField('Please enter your email address', validators=[Email()])
+    password = PasswordField('Enter your pasword', validators=[DataRequired()])
+    submit = SubmitField('Sign in')
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, redirect, flash, request
 from flask_sqlalchemy import SQLAlchemy
-from forms import RegisterForm
+from forms import RegisterForm, LoginForm, UpdateForm
 import creds
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, logout_user, login_required, current_user
 
 #Create the app instance
 app = Flask(__name__)
@@ -11,6 +12,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = creds.secret_key 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:password123@localhost/flaskblog'
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return Users.query.get(user_id)
+
 
 #Define the home page
 @app.route('/')
@@ -44,6 +54,28 @@ def register():
     our_users = Users.query.order_by(Users.date_registered)
     return render_template('register.html', form = form, name =name, our_users=our_users)
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
+def update(id):
+    form = UpdateForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == 'POST':
+        name_to_update.name = request.form['name']
+        name_to_update.username = request.form['username']
+        name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['color']
+
+        try:
+            db.session.commit()
+            flash('Profile has been updated successfully', 'seccess')
+            return render_template('profile.html')
+        except:
+            flash('Something went wrong...try again!', 'danger')
+            return render_template('update.html', form = form, name_to_update = name_to_update)
+    else:
+        return render_template('update.html', form = form, name_to_update = name_to_update)
+
+# Delete user from the databaase
 @app.route('/delete/<int:id>')
 def delete(id):
     user_to_del = Users.query.get_or_404(id)
@@ -60,8 +92,39 @@ def delete(id):
         flash('Something went wrong. Try again.', 'danger')
         return redirect(url_for('register', form = form, name = name, our_users = our_users))
 
+# Create a login route  
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # When the form is submited
+    if form.validate_on_submit():
+        # Check if the user is in the database
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user:
+            if check_password_hash(user.password_hash, form.password.data):
+                login_user(user)
+                flash('Login was successful', 'success')
+                return render_template('profile.html', id=current_user.id)
+            else:
+                    flash('Please check email and password again!', 'danger')
+        else:
+                flash('Please check email and password again!', 'danger')
+    return render_template('login.html', form = form)
+
+# Create a profile page for users
+@app.route('/profile')
+# Prevent users access without being logged in
+@login_required
+def profile():
+    return render_template('profile.html')
+
+
+
+
+
+
 # Defines the Users model 
-class Users(db.Model):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     username = db.Column(db.String(20), nullable=False, unique=True)
